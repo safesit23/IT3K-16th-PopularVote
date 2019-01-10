5f004f9bf3ef98e8407376a998cc26a2e81fe83a
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-10 23:45:51 +0700

    [ADD] Button.js

---
+++ front-end/components/Core/Button.js
@@ -0,0 +1,27 @@
+import { Button as DefaultButton } from 'antd'
+import styled from 'styled-components'
+import fonts from '../../config/fonts'
+import colors from '../../config/colors'
+
+const ButtonPrimary = styled(DefaultButton)`
+  font-size: ${fonts.paragraph};
+  background-color: ${colors.color1};
+  color: #fff;
+  &:hover{
+    background-color: ${colors.primaryDarker};
+    color: #fff;
+  }
+`
+
+export const ButtonSecondary = styled(DefaultButton)`
+  font-size: ${fonts.paragraph};
+  background-color: transparent;
+  color: ${colors.primary};
+  border-color: ${colors.color1};
+  &:hover{
+    background-color: ${colors.color2};
+    color: ${colors.color3}
+  }
+`
+
+export default ButtonPrimary
