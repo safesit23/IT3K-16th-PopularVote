c474139c1927b50f8171d29821638b85b6b91dc4
aofpcc <Bunyarit65@hotmail.co.th>
2019-01-10 20:08:31 +0700

    [ADD] 80 % Complete user vote
    Remain: Unit Test

---
+++ api/database/migrations/2019_01_10_125325_create_competitors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompetitorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('competitors', function (Blueprint $table) {
+            $table->increments('idCompetitor');
+            $table->string('name');
+            $table->string('university');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('competitors');
+    }
+}
