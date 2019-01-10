be5ce50ac96e9b2551f3d5a2abaf5c204ba91865
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-10 23:47:20 +0700

    [UPDATE] add element

--- front-end/components/Login/index.js
+++ front-end/components/Login/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import Logo from '../Core/logo'
+import styled from 'styled-components'
+import { Button } from 'reactstrap';
+// import Button from '../Core/Button'
 // import LoginFaceBook from './LoginFaceBook'
 
 class Login extends React.Component {
@@ -7,8 +12,11 @@ class Login extends React.Component {
       <div className="container-fluid mt-5">
         <div className="row mt-5 justify-content-center">
           <div className="col-8 col-md-4 mt-5">
+            <Logo />
             {/* <LoginFaceBook /> LoginFaceBook */}
             Login
+            <br />
+            <Button color="primary">primary</Button>{' '}
           </div>
         </div>
       </div>
