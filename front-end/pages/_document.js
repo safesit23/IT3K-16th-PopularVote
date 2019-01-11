50062ef45026a4807698ec7e5557a92feac2fc16
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-11 12:27:31 +0700

    [UPDATE] move title to _app.js

--- front-end/pages/_document.js
+++ front-end/pages/_document.js
@@ -11,7 +11,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>IT3K-PopularVote</title>
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
           <script src="https://as.alipayobjects.com/g/component/??es6-shim/0.35.1/es6-sham.min.js,es6-shim/0.35.1/es6-shim.min.js"></script>
