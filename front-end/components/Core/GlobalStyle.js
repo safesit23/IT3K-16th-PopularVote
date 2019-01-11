0bd9a889ed01ff5ee7afbd78fe5f6edcb57bfdbc
Mild-TN <mild20091931@gmail.com>
2019-01-11 21:53:16 +0700

    [ADD] _app.js

---
+++ front-end/components/Core/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  .ant-calendar-month-panel-selected-cell .ant-calendar-month-panel-month {
+    background: #00B2FF !important;
+    color: #fff !important;
+  }
+  body{
+    font-family:'Sarabun', sans-serif !important;
+  }
+`
+
+export default GlobalStyle
