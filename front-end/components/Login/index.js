0c0f359c1a51e9698f82113072522daa769ad0e4
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-12 01:03:08 +0700

    [UPDATE] login page

--- front-end/components/Login/index.js
+++ front-end/components/Login/index.js
@@ -3,31 +3,33 @@ import { Container, Row, Col } from 'reactstrap'
 import Logo from '../Core/logo'
 import styled from 'styled-components'
 import { Button } from 'reactstrap';
-import { Button as AntdButton } from 'antd'
 // import Button from '../Core/Button'
 // import LoginFaceBook from './LoginFaceBook'
 import Color from '../../config/fonts'
 
+const PageContainer = styled(Container)`
+  background-color: balck;
+  height : 1000px;
+`
 
 class Login extends React.Component {
   render () {
     return (
+      <PageContainer fluid>
       <Container>
-        <Row className="mt-5 justify-content-center">
+        <Row className="mt-5 d-flex justify-content-between align-items-center">
           <Col>
             <Logo />
             {/* <LoginFaceBook /> LoginFaceBook */}
           </Col>
         </Row>
-        <Row className="mt-5 justify-content-center">
-          <Col md="3">
-            <Button color="primary">primary</Button>
-          </Col>
-          <Col md={{size:3,offset:2}}>
-           <AntdButton>Default</AntdButton>
+        <Row className="mt-5 d-flex justify-content-between align-items-center">
+          <Col>
+            <Button color="primary">primary</Button>{'  '}
           </Col>
         </Row>
       </Container>
+      </PageContainer>
     )
   }
 }
