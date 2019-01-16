3e1e060db75767bf98e954fe1b3f9894eb6e3b2c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-16 14:59:18 +0700

    [UPDATE] add props font color

--- front-end/components/Core/Text.js
+++ front-end/components/Core/Text.js
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 import config from '../../config/fonts'
 
 
-const Headline = styled.h1`
+export const Headline = styled.h1`
   font-size: ${config.headline};
   font-weight: bold;
+  color: ${props => props.color || "black"};
 `
 export const IT3K = styled.h1`
   font-size:${config.IT3K};
@@ -13,18 +14,22 @@ export const IT3K = styled.h1`
 `
 export const Subtitle = styled.h1`
   font-size: ${config.subtitle};
+  color: ${props => props.color || "black"};
 `
 
 export const Paragraph = styled.h1`
   font-size: ${config.paragraph};
+  color: ${props => props.color || "black"};
 `
 
 export const ParagraphBold = styled(Paragraph)`
-  font-weight: bold;
+  font-weight: bold;  
+  color: ${props => props.color || "black"};
 `
 
 export const Small = styled.h5`
   font-size: ${config.small};
+  color: ${props => props.color || "black"};
 `
 
 export default Headline
