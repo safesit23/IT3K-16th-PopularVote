bfd18f1e82dce4a249b1f73aa0d5a681291262b4
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-16 16:09:19 +0700

    [ADD] Admin Control

---
+++ front-end/components/AdminControl/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Container, Row, Col} from 'reactstrap'
+
+class AdminControl extends React.Component{
+    render(){
+        return(
+            <Container fluid>
+                Admin Control
+            </Container>
+        )
+    }
+}
+
+export default AdminControl;
---
+++ front-end/pages/admincontrol.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Admin from '../components/AdminControl'
+
+class AdminControl extends React.Component {
+    render() {
+        return (
+            <div>
+                <Admin/>
+            </div>
+        )
+    }
+}
+
+export default AdminControl;
