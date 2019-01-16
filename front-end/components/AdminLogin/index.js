e78a810a9ac6ace157772039e3ce6883bedcf2d8
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-16 16:09:07 +0700

    [ADD] Admin Login

---
+++ front-end/components/AdminLogin/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Container, Row, Col} from 'reactstrap'
+
+class AdminLogin extends React.Component{
+    render(){
+        return(
+            <Container fluid>
+                Admin Login
+            </Container>
+        )
+    }
+}
+
+export default AdminLogin;
---
+++ front-end/pages/adminlogin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../components/AdminLogin'
+
+class AdminLogin extends React.Component {
+    render() {
+        return (
+            <div>
+                <Login />
+            </div>
+        )
+    }
+}
+
+export default AdminLogin;
