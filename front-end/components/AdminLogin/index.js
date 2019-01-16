a6dc96a12e5b2076ba7d1a327ebdb99c04a44b49
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-16 16:19:56 +0700

    [UPDATE] add Sign In

--- front-end/components/AdminLogin/index.js
+++ front-end/components/AdminLogin/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import styled from 'styled-components';
-import { Container, Row, Col} from 'reactstrap'
+import {
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
+  } from 'reactstrap';
 
 class AdminLogin extends React.Component{
     render(){
         return(
             <Container fluid>
-                Admin Login
+            <Container>
+                <h2>Sign In</h2>
+        <Form className="form">
+          <Col>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="myemail@email.com"
+                />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+                />
+            </FormGroup>
+          </Col>
+          <Button>Submit</Button>
+        </Form>
+                </Container>
             </Container>
         )
     }
