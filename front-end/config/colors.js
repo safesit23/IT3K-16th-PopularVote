ee484b85516560a1a5d6261587708d41cba7c49a
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-20 15:24:24 +0700

    [UPDATE] add color name in config

--- front-end/config/colors.js
+++ front-end/config/colors.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 const colors = {
     color1 : '#fff',
+    darkSalmon : '#E09F7D',
+    sunsetOrange : '#EF5D60',
+    paradisePink : '#EC4067',
+    darkPurple : '#A01A7D',
+    blackcurrant : '#311847',
     color2 : '#EF5D60',
     color3 : '#E9B6B7',
     violet : '#311847',
