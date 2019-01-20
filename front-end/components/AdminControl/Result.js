8f0acb3d6c411d335772e251dea9a0088b3ae163
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-20 16:15:45 +0700

    [UPDATE] Add Topic Panel

--- front-end/components/AdminControl/Result.js
+++ front-end/components/AdminControl/Result.js
@@ -10,26 +10,35 @@ const Section = styled(Col)`
 	padding : 10px;
 `
 
+const TitlePanel = (props)=>(
+	     <Row>
+			<Col className="d-flex align-items-center">
+				<Title>{props.name}</Title>
+			</Col>
+			<Col className="d-flex align-items-center justify-content-end">
+				<Button>{props.buttonName}</Button>
+			</Col>
+		</Row>
+)
+
 const Result = ()=>(
     <Row className="pt-4 pl-4">
 		<Col xs="12" className="mb-2">
 		    <Headline>Result</Headline>
 		</Col>
-		<Section xs="6" className="mb-2">
-	        <Row>
-				<Col className="d-flex align-items-center">
-					<Title>WEBSITE</Title>
-				</Col>
-				<Col className="d-flex align-items-center justify-content-end">
-					<Button>FETCH</Button>
-				</Col>
-			</Row>
-		</Section>
+
+        <Section xs="6" className="mb-2">
+		    <TitlePanel name="Web" buttonName="bN"/>
+
+        </Section>
+
 		<Section xs="5" className="mb-2">
-			<Title>IT3K</Title>
+            <TitlePanel name="IT3K" buttonName="bN"/>
+
 		</Section>
 		<Section xs="11" className="mb-2">
-			<Title>Popular Vote</Title>
+            <TitlePanel name="PopularVote" buttonName="bN"/>
+
 		</Section>
 	</Row>
 )
