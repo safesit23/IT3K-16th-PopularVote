6d1f397e29a1e9de3d7f6f401662653338995ea5
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-20 15:24:46 +0700

    [UPDATE] add panel in admin control

--- front-end/components/AdminControl/index.js
+++ front-end/components/AdminControl/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import { Container, Row, Col } from 'reactstrap'
+import Button from '../Core/Button'
 import socketIOClient from 'socket.io-client'
-import { Radio } from 'antd';
+import { Radio } from 'antd'
+import {Headline, Subtitle} from '../Core/Text'
+
+const Section = styled(Col)`
+	margin-left : 10px;
+	background-color : ${props => props.color || "#F9F9F9"};
+	padding : 10px;
+`
 
 const RadioGroup = Radio.Group;
-
 
 const socket = socketIOClient('http://localhost:9000')
 
@@ -42,14 +49,46 @@ class AdminControl extends React.Component {
 
 		return (
 			<Container fluid>
-				changePath : 
-				<RadioGroup onChange={this.onChange} value={this.state.value}>
-        <Radio value={1}>Play Vote</Radio>
-        <Radio value={2}>Count Projector</Radio>
-        <Radio value={3}>Play Projector</Radio>
-        <Radio value={4}>Topic Projector</Radio>
-      </RadioGroup>
-				<button onClick={() => this.getPath()}>waiting Test</button>
+					<Row className="pt-4 pl-4">
+						<Col xs="10">
+							<Headline>VOTE Control Panel</Headline>
+						</Col>
+						<Col xs={{size:2}}>
+						<Button>LOGOUT</Button>
+						</Col>
+					</Row>
+
+					<Row className="pl-4">
+						<Section xs="6" color="#FFEEE4">
+								<Subtitle>SETTING</Subtitle>
+								changePath : 
+								<RadioGroup onChange={this.onChange} value={this.state.value}>
+        				<Radio value={1}>Play Vote</Radio>
+        				<Radio value={2}>Count Projector</Radio>
+        				<Radio value={3}>Play Projector</Radio>
+       					<Radio value={4}>Topic Projector</Radio>
+      					</RadioGroup>
+								<button onClick={() => this.getPath()}>waiting Test</button>
+						</Section>
+						<Section xs="5">
+							<Subtitle>USER</Subtitle>
+						</Section>
+					</Row>
+
+					<Row className="pt-4 pl-4">
+						<Col xs="12" className="mb-2">
+							<Headline>Result</Headline>
+						</Col>
+						<Section xs="6" className="mb-2">
+							<Subtitle>WEBSITE</Subtitle>
+						</Section>
+						<Section xs="5" className="mb-2">
+							<Subtitle>IT3K</Subtitle>
+						</Section>
+						<Section xs="11" className="mb-2">
+							<Subtitle>Popular Vote</Subtitle>
+						</Section>
+					</Row>
       </Container>
 		)
 	}
