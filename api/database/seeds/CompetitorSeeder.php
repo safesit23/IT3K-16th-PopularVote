7074b1f92fd13b7c077c2b2f703e9df0bf99ec31
llUJO <jirapan_yankhan@hotmail.com>
2019-01-22 19:57:26 +0700

    [ EDIT ] Update competitor information

--- api/database/seeds/CompetitorSeeder.php
+++ api/database/seeds/CompetitorSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class CompetitorSeeder extends Seeder
 {
     /**
@@ -11,14 +11,51 @@ class CompetitorSeeder extends Seeder
      */
     public function run()
     {
-        $MAX = 6;
-        $competitor_name =  ['นาย จันทร์ ทองดีกว่า', 'นาย จันทร์ ทองดีกว่าที่สุด', 'นายอังคาร ทองดีกว่าที่สุด', 'นาย พุธ ทองดีกว่าที่สุด', 'name5', 'name6'];
-        $university = ['บางมด', 'พระนครเหนือ', 'ลาดกระบัง', 'บางมด', 'พระนครเหนือ', 'ลาดกระบัง'];
-        for($i = 0 ; $i < $MAX ; $i++){
-            DB::table('Competitor')->insert([
-                'name' => $competitor_name[$i],
-                'university' => $university[$i]
-            ]);
-        }
+
+        DB::table('Competitor')->insert([
+            [
+            'name' => 'นายพิรุฬห์ลักษณ์ มโนรัตน์',
+            'nickname' => 'พีรณัฐ',
+            'university' => "พระนครเหนือ",
+            'like' => "0",
+            'share' => "0"
+            ],
+            [
+            'name' => 'นางสาวชนิตา ศิลานฤธานนท์',
+            'nickname' => 'พีช',
+            'university' => "พระนครเหนือ",
+            'like' => "0",
+            'share' => "0"
+            ],
+            [
+            'name' => 'นายสิรภพ รักเกื้อ',
+            'nickname' => 'เจ',
+            'university' => "ลาดกระบัง",
+            'like' => "0",
+            'share' => "0"
+            ],
+            [
+            'name' => 'นาวสาวเบญจพร เงินศรีตระกูล',
+            'nickname' => 'เม',
+            'university' => "ลาดกระบัง",
+            'like' => "0",
+            'share' => "0"
+            ],
+            [
+            'name' => 'นางสาวเกวลิน อินทรกำแหง',
+            'nickname' => 'เกรซ',
+            'university' => "บางมด",
+            'like' => "0",
+            'share' => "0"
+            ],
+            [
+            'name' => 'นายจักราช นิลละออ ',
+            'nickname' => 'เจมส์',
+            'university' => "บางมด",
+            'like' => "0",
+            'share' => "0"
+            ]              
+        ]);
+
     }
 }
