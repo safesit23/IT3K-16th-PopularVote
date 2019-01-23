1937d141d1d020325804ddfe00baf6e85cdd795f
tinypt <PjungSs@hotmail.co.th>
2019-01-24 00:52:33 +0700

    [ADD] logo size 70 for new design

---
+++ front-end/components/Core/logohead.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const Logo = styled.img`
+    width: 100%;
+    @media (min-width:320px) {
+        width: 70px;
+    }
+`
+
+export default class LogoIT3K extends Component {
+    render() {
+        return (
+            <Logo className="mt-5" src="static/img/logoIT3K.svg" alt="Logo IT3K #16"></Logo>
+        );
+    }
+}
