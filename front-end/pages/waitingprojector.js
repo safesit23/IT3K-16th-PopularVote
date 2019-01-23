a5f32bb238b31530430e8f8c33ff2f9290e3dc61
Mild-TN <mild20091931@gmail.com>
2019-01-23 19:09:50 +0700

    [UPDATE] waitingproject.js

--- front-end/pages/waitingprojector.js
+++ front-end/pages/waitingprojector.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import BgColor from '../config/colors'
-import styled from 'styled-components';
 import ENV from '../config/envConfig'
 import socketIOClient from 'socket.io-client'
-
-const Bg = styled.div`
-  background-color : ${BgColor.violet};
-	height: 100vh;
-` 
-class WaitingPage extends React.Component {
+import WaitingPage from '../components/WaitingProjector'
+class Waiting extends React.Component {
 
 	render() {
 		return (
-			<Bg className="d-flex justify-content-center text-white">
-				This is WaitingPage
-			</Bg>
+			<WaitingPage />
 		);
 	}
 }
 
-export default WaitingPage;
+export default Waiting;
