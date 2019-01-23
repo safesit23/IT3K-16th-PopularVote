790af7c2dcdf1bdc46d3afeb2c916a6cf50a49ee
Mild-TN <mild20091931@gmail.com>
2019-01-23 19:08:36 +0700

    [UPDATE] แก้ adminControl

--- front-end/components/AdminControl/index.js
+++ front-end/components/AdminControl/index.js
@@ -18,24 +18,48 @@ const RadioGroup = Radio.Group;
 
 const socket = socketIOClient(ENV.PATH_SOCKET)
 
-const pathname = ['playvoting', 'countprojector', 'playprojector', 'topicprojector','result','waitingprojector']
+const pathname = [{ label: 'Play vote', value: 'playvoting' },
+{ label: 'Result', value: 'result' }]
+
+
+const options = [
+	{ label: 'Topic', value: 'topicprojector' },
+	{ label: 'Waiting', value: 'waitingprojector' },
+	{ label: 'Count', value: 'countprojector' },
+	{ label: 'Vote', value: 'playprojector' },
+	{ label: 'Time Out', value: 'timeoutprojector' },
+];
+
 class AdminControl extends React.Component {
 	state = {
 		pathname: '',
-		value: 1,
+		value: '',
+		value2: '',
 	}
 
 	componentDidMount() {
 	}
 
 	changePath = (pathname) => {
-		socket.emit('changePath', pathname)
+		console.log('Path name : ', pathname)
+		if (pathname != null) {
+			socket.emit('changePath', pathname)
+		}
 	}
+
+	changePathProjector = (projectorPath) => {
+		console.log('Projector Path : ', projectorPath)
+		if (projectorPath != null) {
+			socket.emit('projectorPath', projectorPath)
+		}
+	}
+
 
 	getPath = (e) => {
 		e.preventDefault()
-		console.log(this.state.value)
+		console.log('Tetse.value : ', this.state.value)
 		this.changePath(this.state.value)
+		this.changePathProjector(this.state.value2)
 	}
 
 	onChange = (e) => {
@@ -43,6 +67,17 @@ class AdminControl extends React.Component {
 		this.setState({
 			value: e.target.value,
 		});
+		let value = this.state.value
+		this.changePath(value)
+	}
+
+	onChange2 = (e) => {
+		console.log('radio2 checked', e.target.value);
+		this.setState({
+			value2: e.target.value,
+		});
+		let value2 = this.state.value2
+		this.changePathProjector(value2)
 	}
 
 	render() {
@@ -50,6 +85,8 @@ class AdminControl extends React.Component {
 		return (
 			<Container fluid>
 				<Row className="pt-4 pl-4">
+					{/* <RadioGroup options={pathname} onChange={this.onChange1} value={this.state.value1} /> */}
+					{/* <RadioGroup options={options} onChange={this.onChange2} value={this.state.value2} /> */}
 					<Col xs="10">
 						<Headline>VOTE Control Panel</Headline>
 					</Col>
@@ -60,16 +97,23 @@ class AdminControl extends React.Component {
 				<Row className="pl-4">
 					<Section xs="6" color="#FFEEE4">
 						<Title>SETTING</Title>
-						changePath :
-								<form onSubmit={this.getPath}>
-							<RadioGroup onChange={this.onChange} value={this.state.value}>
-								<Radio value={pathname[0]}>Play Vote</Radio>
-								<Radio value={pathname[1]}>Count Projector</Radio>
+						<form onSubmit={this.getPath}>
+							Change Path : <br />
+							<RadioGroup options={pathname} onChange={this.onChange} value={this.state.value} />
+							<br />
+							Change Path Projector : <br />
+							<RadioGroup options={options} onChange={this.onChange2} value={this.state.value2} />
+							<br />
+							Path user : {this.state.value}
+							<br />
+							Path projector : {this.state.value2}
+							<br />
+							{/* <Radio value={pathname[1]}>Count Projector</Radio>
 								<Radio value={pathname[2]}>Play Projector</Radio>
 								<Radio value={pathname[3]}>Topic Projector</Radio>
 								<Radio value={pathname[4]}>Result</Radio>
-								<Radio value={pathname[5]}>Waiting projector</Radio>
-							</RadioGroup>
+								<Radio value={pathname[5]}>Waiting projector</Radio> */}
+							{/* </RadioGroup> */}
 							<input type="submit" value="hitme" />
 						</form>
 					</Section>
