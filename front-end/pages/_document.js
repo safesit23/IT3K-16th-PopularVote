0f1aa9ecfa116fe3d08108ea7a9a3889e0dbba62
Mild-TN <mild20091931@gmail.com>
2019-01-23 19:07:08 +0700

    [UPDATE] charSet

--- front-end/pages/_document.js
+++ front-end/pages/_document.js
@@ -24,7 +24,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
-          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.12.3/antd.min.css" />
           <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet"></link>
