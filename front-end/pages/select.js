65b14d63525c031235c0256270070c3cf79df169
tinypt <PjungSs@hotmail.co.th>
2019-01-24 23:02:21 +0700

    [EDIT] change select to new design

--- front-end/pages/select.js
+++ front-end/pages/select.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import BgColor from '../config/colors'
-import SelectSlick from '../components/Select'
+import SelectSlickold from '../components/Select'
+import SelectSlick from '../components/Select2'
 
 class Select extends React.Component {
   render() {
