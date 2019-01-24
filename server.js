0fa7af52ae221b752d9af7b7125699bd7b43a039
Mild-TN <mild20091931@gmail.com>
2019-01-24 20:06:11 +0700

    [UPDATE] count user of socket.io

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 const port = 9000
+let count = 0;
+// count += 1;
 
 io.on('connection', function (socket) {
-  console.log('connect')
+  count++
+  console.log('user connect : ',count)
+  socket.emit('countUser',count)
   socket.on('changePath', (pathName) => {
     console.log('Path User : ', pathName);
     io.sockets.emit('pathName', pathName);
     io.sockets.emit('pathNameResult', pathName);
+    // count += 1;
   });
   socket.on('projectorPath', (pathNameProj) => {
     console.log('Path Projector : ',pathNameProj);
     io.sockets.emit('pathProjector',pathNameProj);
+  })
+  socket.on('getRound',(round) => {
+    console.log('Rund : ',round)
+    io.sockets.emit('round',round);
   })
 });
 
