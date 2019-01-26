e88b766b24714d9d0a7ee76e734b287ba0505c26
Mild-TN <mild20091931@gmail.com>
2019-01-26 12:48:05 +0700

    [ADD] CompetitorRepository.php

---
+++ api/app/Repositories/CompetitorRepositpry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\CompetitorRepository;
+use App\Repositories\CompetitorRepositoryInterface;
+
+class CompetitorRepository implements CompetitorRepositoryInterface
+{
+  
+}
