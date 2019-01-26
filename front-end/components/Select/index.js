d5ab026b8ebe33fc1f3ed1766d610399bafac3f4
tinypt <PjungSs@hotmail.co.th>
2019-01-26 19:58:43 +0700

    [UPDATE] ย้าย select 2 ไปไว้ใน folder เดียวกัน

--- front-end/components/Select/index.js
+++ front-end/components/Select/index.js
@@ -1,24 +1,42 @@
-import React from 'react'
-import { Button, Container, Row } from 'reactstrap'
-import Slick from './MoblieSelect'
-import styled from 'styled-components'
-import BgColor from '../../config/colors'
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import styled from "styled-components";
+import Logo from "../Core/logohead";
+import BgColor from "../../config/colors";
+import { Subtitle } from "../Core/Text";
+import CardVote from "./CardVote"
 
 const Bg = styled.div`
-  background-image:${BgColor.background};
+  background-image: ${BgColor.backgroundnew};
   height: 100vh;
-`
+`;
 
 class Index extends React.Component {
   render() {
     return (
-      <Bg>
-        <Container>
-          <Slick />
+        <Bg>
+        <Container fluid>
+          <Container>
+            <Row>
+              <Col className="text-center">
+                <Logo />
+              </Col>
+            </Row>
+            <Row>
+              <Col className="d-flex justify-content-center mt-2">
+                <Subtitle className="text-white">เลือกผู้เข้าแข่งขันที่ต้องการโหวต</Subtitle>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CardVote block />
+              </Col>
+            </Row>
+          </Container>
         </Container>
       </Bg>
-    )
+    );
   }
 }
 
-export default Index
+export default Index;
