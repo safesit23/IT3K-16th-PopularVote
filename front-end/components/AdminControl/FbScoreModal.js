adc09a0db4582ed7b8b3c8176922252f9888723c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-01-27 21:28:43 +0700

    [UPDATE] Edit Modal

--- front-end/components/AdminControl/FbScoreModal.js
+++ front-end/components/AdminControl/FbScoreModal.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Container, Row, Col, Table} from 'reactstrap';
+import { InputNumber } from 'antd';
+import FbScoreService from '../../service/FacebookScoreService'
 
 class FacebookScoreModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      facebook: {
+        1: [0, 0],
+        2: [0, 0],
+        3: [0, 0],
+        4: [0, 0],
+        5: [0, 0],
+        6: [0, 0],
+      },
     };
 
     this.toggle = this.toggle.bind(this);
   }
 
   toggle() {
+    this.sendFBScore()
     this.setState({
       modal: !this.state.modal
     });
+  }
+
+  sendFBScore(){
+    FbScoreService.sendFBScore(this.state.facebook)
   }
 
   render() {
@@ -22,13 +37,25 @@ class FacebookScoreModal extends React.Component {
       <div>
         <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalHeader toggle={this.toggle}>FACEBOOK IT3K</ModalHeader>
           <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Table>
+						<thead>
+							<th>Name</th>
+							<th>University</th>
+							<th>Like</th>
+							<th>Share</th>
+						</thead>
+						<tr>
+							<td>Name 1</td>
+							<td>บางมด</td>
+							<td><InputNumber min={0}/></td>
+							<td><InputNumber min={0} /></td>
+						</tr>
+					</Table>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            <Button color="primary" onClick={this.toggle}>SAVE</Button>
           </ModalFooter>
         </Modal>
       </div>
