8a4b7e8d4ae4f9612c134aa1adbef4657dbed12b
tinypt <PjungSs@hotmail.co.th>
2019-01-27 19:14:21 +0700

    [UPDATE] ปรับตาม design แต่ยังได้ nav ไม่เต็มหน้า

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,29 +1,33 @@
-import React from 'react'
-import Login from './../components/Login'
-import styled from 'styled-components'
-import BgColor from '../config/colors'
-import Nav from '../components/Core/Nav'
-import { Container, Row, Col } from 'reactstrap'
+import React from "react";
+import Login from "./../components/Login";
+import styled from "styled-components";
+import BgColor from "../config/colors";
+import Nav from "../components/Core/Nav";
+import { Container, Row, Col } from "reactstrap";
+import Logo from "../components/Core/logohead";
+import { TitleWh } from "../components/Core/Text";
 
 const Bg = styled(Col)`
-  background:${BgColor.backgroundnew};
+  background: ${BgColor.backgroundnew};
   height: 100vh;
-`
+`;
 class componentName extends React.Component {
   render() {
     return (
-      <div>
-        <Container >
-        <Nav />
-          <Row>
-            <Bg>
-              <Login />
-            </Bg>
-          </Row>
-        </Container>
-      </div>
-    )
+      <Container fluid >
+          <Nav />
+        <Row>
+          <Bg>
+            <Col className="text-center">
+              <Logo />
+            </Col>
+            <TitleWh className="text-center">ระบบโหวต ดาว - เดือน</TitleWh>
+            <Login />
+          </Bg>
+        </Row>
+      </Container>
+    );
   }
 }
 
-export default componentName
+export default componentName;
