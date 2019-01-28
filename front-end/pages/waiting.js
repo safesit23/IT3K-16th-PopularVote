0d36f24e45dcc554991856eee5ad4d1416a67606
tinypt <PjungSs@hotmail.co.th>
2019-01-28 21:37:49 +0700

    [UPDATE] add bg, text

--- front-end/pages/waiting.js
+++ front-end/pages/waiting.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Waiting from '../components/Waiting'
+import Nav from "./../components/Core/Nav";
+
 class WaitingPage extends React.Component {
 	render() {
 		return (
 			<div>
+				<Nav/>
 				<Waiting />
 			</div>
 		);
