9150b002d0d701434b1cbda9a8660a8f65813532
tinypt <PjungSs@hotmail.co.th>
2019-01-28 20:07:35 +0700

    [UPDATE] แก้ไข Card ให้มีขนาดเท่ากัน

--- front-end/components/Core/Card.js
+++ front-end/components/Core/Card.js
@@ -1,22 +1,29 @@
 import React from "react";
-import { Card,Row,Col } from "reactstrap";
-import { SubCaption, Caption, Body } from '../Core/Text';
-import Pic from '../Core/Picture'
+import { Card, Row, Col, Container } from "reactstrap";
+import { SubCaption, Caption, Body } from "../Core/Text";
+import Pic from "../Core/Picture";
 
 class CardVote extends React.Component {
-  
   render() {
     return (
       <div>
         <Card>
           <Row>
-            <Col className="col-9">
-              <Body className="m-2">{this.props.name}</Body>
-              <SubCaption className="m-2">{this.props.nickname}</SubCaption>
-              <Caption className="m-2">{this.props.university}</Caption>
+            <Col xs="8" className="text-left mr-0 mt-2">
+              <Row>
+                <Caption className="ml-4 mt-2">{this.props.university}</Caption>
+              </Row>
+              <Row>
+                <Body className="ml-4 mb-1 mr-0">{this.props.name}</Body>
+              </Row>
+              <Row>
+                <SubCaption className="ml-4 mb-2">
+                  {this.props.nickname}
+                </SubCaption>
+              </Row>
             </Col>
-            <Col className="col-3 text-right">
-              <Pic pic={this.props.id} widthPic='20vw'/>
+            <Col xs="4">
+              <Pic pic={this.props.id} width="100%" />
             </Col>
           </Row>
         </Card>
