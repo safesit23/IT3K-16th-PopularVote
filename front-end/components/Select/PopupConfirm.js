16ec0400a9caac6d8a4f6d691626d0fdbae8e7bc
tinypt <PjungSs@hotmail.co.th>
2019-01-29 18:08:41 +0700

    [UPDATE] ปรับฟ้อนตาม ui

--- front-end/components/Select/PopupConfirm.js
+++ front-end/components/Select/PopupConfirm.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
-import styled from 'styled-components'
-import Router from 'next/router'
-
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import styled from "styled-components";
+import { TitleBl, Caption, Body, Paragraph } from "../Core/Text";
+import Router from "next/router";
 
 const Popup = styled.div`
   position: fixed;
@@ -13,70 +13,92 @@ const Popup = styled.div`
   right: 0;
   bottom: 0;
   margin: auto;
-  background-color: rgba(0,0,0, 0.5);
-`
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 const PuopupInner = styled.div`
   position: absolute;
   left: 10%;
   right: 10%;
   top: 25%;
-  bottom: 50%;
+  bottom: 40%;
   margin: auto;
   background: white;
-  border:0;
-
-`
+  border: 0;
+  box-shadow: 0px 19px 38px rgba(0, 0, 0, 0.3),
+    0px 15px 12px rgba(0, 0, 0, 0.22);
+  border-radius: 2px;
+`;
 
 const CloseButton = styled.button`
   background-color: transparent;
-  border:0px;
-`
+  border: 0px;
+`;
 
 const ConfirmBtn = styled.button`
   background-color: transparent;
-  border:0px;
-    :hover{
+  border: 0px;
+  :hover {
     background-color: #311847;
     width: 100%;
-    border:0px;
+    border: 0px;
     color: #ffffff;
     height: 6vh;
     bottom: 0;
-    }
-`
+  }
+`;
 
 class PopupConfirm extends React.Component {
-
   state = {
-    path: ''
-  }
+    path: ""
+  };
 
   changePath = async () => {
     await Router.push({
-      pathname: '/waiting',
+      pathname: "/waiting",
       query: { id: `${this.props.id}`, name: `${this.props.name}` }
-    })
-  }
+    });
+  };
 
   render() {
-    console.log(this.state.path, 'path name');
-    console.log(this.props.id)
+    console.log(this.state.path, "path name");
+    console.log(this.props.id);
     return (
       <Popup>
         <PuopupInner>
-          <h5 className="d-flex justify-content-center mt-4"> ต้องการเลือก </h5>
-          <p className=" d-flex justify-content-center">{this.props.name} หรือไม่ ?</p>
-          <Row className=" d-flex justify-content-center" >
+          <TitleBl className="text-left mt-4 ml-4"> ยืนยันการโหวต </TitleBl>
+          <Paragraph className="text-left ml-4 mr-4">
+            {this.props.name}
+            <br /> หรือไม่ ?
+          </Paragraph>
+          <Row className="text-left ml-4 mr-4 mb-4">
+            <img src="../../static/img/info.png" width="10px" height="10px" />
+            <Caption color="#E2574C" className="pl-1">
+              การเลือกครั้งนี้จะไม่สามารถกลับมาแก้ไขได้
+            </Caption>
+          </Row>
+          <Row className=" d-flex justify-content-center">
+            <Col />
             <Col>
-              <CloseButton className="align-self-end float-right" onClick={this.props.closePopup}>ยกเลิก</CloseButton>
+              <CloseButton
+                className="align-self-end float-right"
+                onClick={this.props.closePopup}
+              >
+                <Body color="#A01A7D">ยกเลิก</Body>
+              </CloseButton>
             </Col>
             <Col>
-              <ConfirmBtn className="mb-0" id={this.props.id} onClick={() => this.changePath()} >ยืนยัน</ConfirmBtn>
+              <ConfirmBtn
+                className="mb-0"
+                id={this.props.id}
+                onClick={() => this.changePath()}
+              >
+                <Body color="#A01A7D">ยืนยัน</Body>
+              </ConfirmBtn>
             </Col>
           </Row>
         </PuopupInner>
       </Popup>
-    )
+    );
   }
 }
-export default PopupConfirm
+export default PopupConfirm;
