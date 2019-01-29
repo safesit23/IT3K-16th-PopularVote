f542e4061de8842dd0c8aed5446cc52139fb8dc8
tinypt <PjungSs@hotmail.co.th>
2019-01-29 18:03:34 +0700

    [UPDATE] create new text and add shadow

--- front-end/components/Core/logo.js
+++ front-end/components/Core/logo.js
@@ -5,7 +5,8 @@ const Logo = styled.img`
     width: 100%;
     @media (min-width:320px) {
         width: 50%;
-    }
+	}
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `
 
 export default class LogoIT3K extends Component {
