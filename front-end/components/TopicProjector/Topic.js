6cdc20c9a533effc79898407921175eca7daf18a
tinypt <PjungSs@hotmail.co.th>
2019-01-31 00:12:17 +0700

    [UPDATE] ปรับตาม ui topic

--- front-end/components/TopicProjector/Topic.js
+++ front-end/components/TopicProjector/Topic.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import Logo from '../Core/logo'
-import { IT3K, Headline, Subtitle } from '../Core/Text'
+import { IT3K, WebWh, HeadlineWh } from '../Core/Text'
 import { Container, Row, Col } from 'reactstrap'
-import BgColor from '../../config/colors'
-import styled from 'styled-components';
-
 
 class TopicProjector extends React.Component {
   render() {
@@ -23,14 +20,14 @@ class TopicProjector extends React.Component {
           </Row>
           <Row>
             <Col md={{ size: 6, offset: 3 }}>
-              <Subtitle color="white" className="d-flex justify-content-center text-center">
+              <HeadlineWh className="d-flex justify-content-center text-center">
                 2 กุมภาพันธ์ 2019 <br /> ณ มหาวิทยาลัยพระจอมเกล้าธนบุรี
-                </Subtitle>
+                </HeadlineWh>
             </Col>
           </Row>
           <Row>
-            <Col md={{ size: 6, offset: 3 }}>
-              <Headline color="white" className="d-flex justify-content-center mt-5">it3k.in.th</Headline>
+            <Col>
+              <WebWh className="d-flex justify-content-center mt-5">it3k.in.th</WebWh>
             </Col>
           </Row>
         </Container>
