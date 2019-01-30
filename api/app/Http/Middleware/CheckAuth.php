d3e80fbbfc073498b6ecd466be2f75d569237559
llUJO <jirapan_yankhan@hotmail.com>
2019-01-30 17:54:14 +0700

    [ EDIT ] Check Authen midleware

--- api/app/Http/Middleware/CheckAuth.php
+++ api/app/Http/Middleware/CheckAuth.php
@@ -4,25 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class CheckAuth 
+class CheckAuth
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    private $encode_key = "IT3K-naja";
+
+    private $AdminSecret = "Nongmoonoum";
 
     public function handle($request, Closure $next)
     {
         $auth = $request->header('authorization');
         $token = substr($auth, 7);
         $decodedJwt = \Lindelius\JWT\JWT::decode($token);
-        $decodedJwt->verify($this->encode_key);
-        $provider_id = $decodedJwt->sub->provider_id;
-        $round = $decodedJwt->sub->round;
-        return $next($request);
+        $decodedJwt->verify($this->AdminSecret);
+        $response = $next($request);
+    
+        return $response;
+ 
+
+        
     }
 }
