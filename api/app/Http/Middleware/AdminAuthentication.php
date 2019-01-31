4176c9da161c73dde29478e551f61019030ab108
llUJO <jirapan_yankhan@hotmail.com>
2019-01-31 23:10:11 +0700

    [ EDIT ] Remove return

--- api/app/Http/Middleware/AdminAuthentication.php
+++ api/app/Http/Middleware/AdminAuthentication.php
@@ -20,9 +20,9 @@ class AdminAuthentication
         $token = substr($auth, 7);
         $decodedJwt = \Lindelius\JWT\JWT::decode($token);
         $decodedJwt->verify($this->AdminSecret);
-        $response = $next($request);
-    
-        return $response;
+       
+        return $next($request);
+   
  
 }
 }
