d517195300e60b70be0ef50afbf7b2b3ef434076
llUJO <jirapan_yankhan@hotmail.com>
2019-01-31 17:10:27 +0700

    [ EDIT ]  Format user factor

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -15,10 +15,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'provider_id' => $faker->randomNumber(6),
+        'round' => $faker->randomElement(['1', '2']),
+        'idCompetitor' => $faker->numberBetween(1, 6),
+        'count' => $faker->numberBetween(1, 200)
     ];
 });
