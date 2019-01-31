dccf3e16bc2620adb8c32879926287aaa596689b
tinypt <PjungSs@hotmail.co.th>
2019-02-01 00:48:45 +0700

    [ADD] create file for ResultProjector รอ design

---
+++ front-end/pages/resultprojector.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Result from '../components/ResultProjector'
+class ResultPage extends React.Component {
+
+	render() {
+		return (
+			<Result />
+		);
+	}
+}
+
+export default ResultPage;
