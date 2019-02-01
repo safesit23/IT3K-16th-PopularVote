fa3e6aa57b3cf9d5d287791fedec36219302eec2
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-01 17:16:08 +0700

    [UPDATE] edit BG TopicProjector

--- front-end/components/TopicProjector/index.js
+++ front-end/components/TopicProjector/index.js
@@ -5,11 +5,31 @@ import Topic from './Topic'
 import socketIOClient from 'socket.io-client'
 import ENV from '../../config/envConfig'
 import Router from 'next/router'
-import FooterTest from '../Core/Footer'
 
-const Bg = styled.div`
-  background: ${BgColor.backgroundnew};
+const Landing = styled.div`
+  background: linear-gradient(220deg, #F3BBAB, #C49AAB, #C49AAB);
+  background-size: 600% 600%;
   height: 100vh;
+
+-webkit-animation: AnimationName 30s ease infinite;
+-moz-animation: AnimationName 30s ease infinite;
+animation: AnimationName 3.5s ease infinite;
+
+@-webkit-keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@-moz-keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+@keyframes AnimationName {
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
 `
 
 const socket = socketIOClient(ENV.PATH_SOCKET)
@@ -30,10 +50,9 @@ class TopicProjector extends React.Component {
 
 	render() {
 		return (
-			<Bg>
+			<Landing>
 				<Topic />
-				<FooterTest mtop='-72%'/>
-			</Bg>
+			</Landing>
 		)
 	}
 }
