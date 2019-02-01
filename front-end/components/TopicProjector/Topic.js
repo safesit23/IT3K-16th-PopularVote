824c0e7da6fe94db7be81741cedbee3e6bef420f
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-02-01 17:37:37 +0700

    [UPDATE] edit content

--- front-end/components/TopicProjector/Topic.js
+++ front-end/components/TopicProjector/Topic.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Logo from '../Core/logo'
-import { IT3K, WebWh, HeadlineWh } from '../Core/Text'
+import { IT3K, WebWh, HeadlineWh,Title } from '../Core/Text'
 import { Container, Row, Col } from 'reactstrap'
+import Footer from '../Core/Footer'
+import styled from 'styled-components'
 
+const Landing = styled(Container)`
+  height: 100vh;
+  width : 100%;
+  left: 0;
+  bottom: 0;
+  position: fixed;
+`
 class TopicProjector extends React.Component {
   render() {
     return (
-      <Container fluid>
+      <Landing fluid>
         <Container>
           <Row>
             <Col className="d-flex justify-content-center mt-5">
@@ -15,23 +24,24 @@ class TopicProjector extends React.Component {
           </Row>
           <Row>
             <Col className="d-flex justify-content-center mt-5">
-              <IT3K>IT 3KINGS #16</IT3K>
+              <WebWh size="40px">IT3K ครั้งที่ 16</WebWh>
             </Col>
           </Row>
           <Row>
-            <Col md={{ size: 6, offset: 3 }}>
-              <HeadlineWh className="d-flex justify-content-center text-center">
-                2 กุมภาพันธ์ 2019 <br /> ณ มหาวิทยาลัยพระจอมเกล้าธนบุรี
+            <Col md={{ size: 12}}>
+              <HeadlineWh size="28px" className="d-flex justify-content-center text-center">
+                2 กุมภาพันธ์ 2019 <br /> ณ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
                 </HeadlineWh>
             </Col>
           </Row>
           <Row>
             <Col>
-              <WebWh className="d-flex justify-content-center mt-5">vote.it3k.in.th</WebWh>
+              <WebWh className="d-flex justify-content-center mt-3">vote.it3k.in.th</WebWh>
             </Col>
           </Row>
         </Container>
-      </Container >
+        <Footer />
+      </Landing>
     )
   }
 }
